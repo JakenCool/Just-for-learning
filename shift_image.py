56e910c2396784164b976c726b172b443c1e8304
JakenCool <409265705@qq.com>
2018-08-06 21:24:10 +0800

    将图形平移，可以使得某一部分画在图形中央

--- shift_image.py
+++ shift_image.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #将图形平移，可以使得某一部分画在图形中央
 
 a=[0,1,2,3]
-b=np.roll(a,1)
-print(b)
-plt.xticks(a,b)
+b=np.roll(a,1)#b = [3 0 1 2]
+
+plt.xticks(a,b) #将x坐标的a换成b
 plt.plot(a,b)
